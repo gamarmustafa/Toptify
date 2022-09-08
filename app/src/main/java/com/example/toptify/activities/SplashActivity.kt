9efd2ad19d8836365262af040313f0447712965b
Gamar Mustafa <gamarmustafa@mail.ru>
2022-09-08 15:00:55 +0400

    set up splash screen

---
+++ app/src/main/java/com/example/toptify/activities/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.toptify.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.WindowManager
+import com.example.toptify.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed(
+            {
+                Log.i("Hello","There")
+                val intent = Intent(this@SplashActivity, IntroActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, 2000
+        )
+    }
+}
