6c34d455dc4d6f73a559ae530773a4683582d57b
Gamar Mustafa <gamarmustafa@mail.ru>
2022-09-08 18:21:18 +0400

    set up the layouts of the activities

---
+++ app/src/main/java/com/example/toptify/activities/TopGenres.kt
@@ -0,0 +1,12 @@
+package com.example.toptify.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.toptify.R
+
+class TopGenres : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_top_genres)
+    }
+}
---
+++ app/src/main/java/com/example/toptify/activities/TopTracksActivity.kt
@@ -0,0 +1,12 @@
+package com.example.toptify.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.toptify.R
+
+class TopTracksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_top_tracks)
+    }
+}
